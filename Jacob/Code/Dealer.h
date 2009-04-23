2556f05f5505512a6f4c5d334d7de369b1806d1b
Jacob Sarvis <jsarvis@users.noreply.github.com>
2009-04-23 02:46:31 +0000

    Added RecipientMsg.h and updated makefile

--- Jacob/Code/Dealer.h
+++ Jacob/Code/Dealer.h
@@ -14,6 +14,7 @@
 #include "Player.h"
 #include "StockPile.h"
 #include "DiscardPile.h"
+#include "RecipientMsg.h"
 
 using namespace std;
 using namespace SimMgmt;
@@ -52,9 +53,9 @@ public:
 
 	void StartGame();
 
-	Message * AcceptDrawStockPile();
+	RecipientMsg * AcceptDrawStockPile(Player * pP_Requester);
 
-	Message * AcceptDrawDiscardPile();
+	RecipientMsg * AcceptDrawDiscardPile(Player * pP_Requester);
 
 	Message * AcceptSorted();
 
@@ -68,12 +69,12 @@ public:
 
 	Message * AcceptShuffleComplete();
 
-	Message * AcceptQueryTopCard();
+	RecipientMsg * AcceptQueryTopCard(Player * pP_Requester);
 
 private:
-	void doAcceptDrawStockPile();
+	void doAcceptDrawStockPile(Player * pP_Requester);
 
-	void doAcceptDrawDiscardPile();
+	void doAcceptDrawDiscardPile(Player * pP_Requester);
 
 	void doAcceptSorted();
 
@@ -87,7 +88,7 @@ private:
 
 	void doAcceptShuffleComplete();
 
-	void doAcceptQueryTopCard();
+	void doAcceptQueryTopCard(Player * pP_Requester);
 
 };
 
---
+++ Jacob/Code/RecipientMsg.h
@@ -0,0 +1,25 @@
+#ifndef RECIPIENTMSG_H
+#define RECIPIENTMSG_H
+
+#include <string>
+#include <vector>
+
+#include "SimMgmt.h"
+
+using namespace std;
+using namespace SimMgmt;
+
+namespace SimModels {
+
+class RecipientMsg: public Message {
+protected:
+	virtual void Put(ostream& fout);
+
+public:
+	Player * pP_Requester
+	RecipientMsg(int Handler, string Description, Player * pP_Requester);
+	virtual void Insert(ostream& fout);
+};
+
+}
+#endif
